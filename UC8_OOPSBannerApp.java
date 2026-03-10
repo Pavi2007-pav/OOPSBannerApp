94a6a55363781163f2dcdff0f1611dbb66882dbc
Pavi2007-pav <pr5074@srmist.edu.in>
2026-03-10 13:13:22 +0530

    uc8

---
+++ UC8_OOPSBannerApp.java
@@ -0,0 +1,61 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class OOPSBannerApp {
+
+    // Function to render banner
+    public static void renderBanner(String word, Map<Character, String[]> patternMap) {
+
+        int rows = 5;
+
+        for (int i = 0; i < rows; i++) {
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                String[] pattern = patternMap.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[i]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // HashMap to store character patterns
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                "*****  ",
+                "*    * ",
+                "*****  ",
+                "*      ",
+                "*      "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        });
+
+        // Word to print
+        String bannerWord = "OOPS";
+
+        // Call function
+        renderBanner(bannerWord, patternMap);
+    }
+}
